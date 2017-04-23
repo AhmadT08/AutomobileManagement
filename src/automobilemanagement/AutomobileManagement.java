c310581bef3e00eaf52ff53f638fd71f485367f8
amr shawky <amr shawky>
2017-04-23 15:19:07 +0200

    added class car

--- src/automobilemanagement/AutomobileManagement.java
+++ src/automobilemanagement/AutomobileManagement.java
@@ -16,6 +16,7 @@ public class AutomobileManagement {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hahahah");
     }
     
 }
