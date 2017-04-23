45d0cf86d375e4060b39427c4d53fade981b0bce
Amrosamaa <amr126141@bue.edu.eg>
2017-04-23 16:42:44 +0200

    osama

--- src/automobilemanagement/AutomobileManagement.java
+++ src/automobilemanagement/AutomobileManagement.java
@@ -16,7 +16,7 @@ public class AutomobileManagement {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("dsfaioj");
+        System.out.println("osama");
     }
     
 }
