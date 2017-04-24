9514f3747884f82ea4b52d42184cb4fff5d10b9a
amrshawkyy <amr123921@bue.edu.eg>
2017-04-24 16:52:12 +0200

    donesasd

--- src/automobilemanagement/AutomobileManagement.java
+++ src/automobilemanagement/AutomobileManagement.java
@@ -17,8 +17,6 @@ public class AutomobileManagement {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        System.out.println("shawky");
-        System.out.println("Battout");
 
     }
     
